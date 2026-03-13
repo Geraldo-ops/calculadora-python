c50f6c2c38642ce13fd7c97f554fe0deaf6fc003
Dev. Geraldo <70671518+Geraldo-ops@users.noreply.github.com>
2026-03-13 19:41:03 -0300

    Calculadora

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+print("Calculadora simples")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o segundo número: "))
+
+soma = num1 / num2 * num3
+
+print("Divisão:", soma)
+print("Multiplicação:", soma)
